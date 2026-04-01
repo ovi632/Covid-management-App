817a33e4dfc2a6fe9e75ae5ee1d372225d3f155e
Ashraful Islam Ovi <ashrafovi2017@gmail.com>
2026-04-01 13:18:16 +0600

    sec commit

---
+++ src/main/java/com/example/covidmanagementapp/LoginController.java
@@ -0,0 +1,41 @@
+package com.example.covidmanagementapp;
+
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private TextField newUserInputTF;
+    @javafx.fxml.FXML
+    private PasswordField checkNewPassTF;
+    @javafx.fxml.FXML
+    private Label createAccountLabel;
+    @javafx.fxml.FXML
+    private ComboBox userInputCB;
+    @javafx.fxml.FXML
+    private CheckBox showPassCheckBox;
+    @javafx.fxml.FXML
+    private PasswordField newPassInputTF;
+    @javafx.fxml.FXML
+    private Label loginLabel;
+    @javafx.fxml.FXML
+    private PasswordField passInputTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void createNewAccountButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void showPassword(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void forgotPassButtonOA(ActionEvent actionEvent) {
+    }
+}
