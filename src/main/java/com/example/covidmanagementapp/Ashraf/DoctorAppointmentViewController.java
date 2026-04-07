54f3f97a2f9f5768012e2cc22b2ce0af0091e760
Ashraful Islam Ovi <ashrafovi2017@gmail.com>
2026-04-07 15:40:28 +0600

    "2nd code"

---
+++ src/main/java/com/example/covidmanagementapp/Ashraf/DoctorAppointmentViewController.java
@@ -0,0 +1,36 @@
+package com.example.covidmanagementapp.Ashraf;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+public class DoctorAppointmentViewController
+{
+    @javafx.fxml.FXML
+    private ComboBox appoinmentTypeCB;
+    @javafx.fxml.FXML
+    private Label notificationLabel;
+    @javafx.fxml.FXML
+    private ComboBox doctorListCB;
+    @javafx.fxml.FXML
+    private DatePicker appoitmentDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox timeSlotCB;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitAppoinmentButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backToDeshboardButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void checkAvailabityButtonOA(ActionEvent actionEvent) {
+    }
+}
